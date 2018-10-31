4c443a9e8b0beccd3299984779d9ad54506e1877
EconClass <bizmail327@gmail.com>
2018-10-30 18:48:20 -0700

    fin histogram

--- analyze_text.py
+++ analyze_text.py
@@ -4,16 +4,24 @@ import collections
 
 def clean_text(source_text):
     '''
-    Removes empty spaces, punctuations and special characters
+    This function accesses a source file and uses punctuation_table to 
+    remove punctuations and special characters.
     '''
-    punctuation_table = str.maketrans( '\n-' , '  ', '''1234567890~!@#$.,%^&*()_+?/`[];'":|''' )
 
-    text = open('the_book.txt')
+    # Creates table of punctuations for comparison
+    punctuation_table = str.maketrans( '\n-' , '  ', '''1234567890~!@#$.,%^&*()_+?/`[];'":|''' )
+    
+    # Opens file and removes characters based on punctuation_table
+    text = open(source_text)
     text_list = text.read().translate(punctuation_table).replace('--', ' ')
     text.close()
     
+    # Splits text into list and lowers case of all words
     text_list = text_list.lower().split()
+
+    # Creates NEW list of same words with out whitespace on ends
     text_list = [ word.strip() for word in text_list ]
+
     return text_list
 
 def histogram(cleaned_text):
@@ -22,7 +30,9 @@ def histogram(cleaned_text):
     and return a histogram data structure that stores each unique word along with
     the number of times the word appears in the source text.
     '''
-    hist_obj = collections.Counter(clean_text)
+    # Creates a dictionary of 'key: value' pairs where  
+    # unique words are keys and occurances are values
+    hist_obj = collections.Counter(cleaned_text)
     return hist_obj
 
 def unique_words(histogram_in):
@@ -32,18 +42,26 @@ def unique_words(histogram_in):
     '''
     return len(histogram_in)
 
-def frequency(word, histogram_in):
+def frequency( histogram_in, word ):
     '''
     This function takes a word and histogram argument and
     returns the number of times that word appears in a text.
     '''
-    if hasattr(histogram_in, word):
-        return getattr(histogram_in, word)
+    if word in histogram_in:
+        return histogram_in[word]
     else: return 0
+   
 
 
 if __name__ == "__main__":
+    source = str(sys.argv[1])
+    find_word = str(sys.argv[2])
 
-    text = clean_text('the_book.txt')
-    print(text)
-    # hist_text = histogram(text)
+    text = clean_text(source)
+    hist_text = histogram(text)
+    
+    not_same = unique_words(hist_text)
+    print('There are {} unique words in the text.'.format(not_same))
+
+    num_word = frequency(hist_text, find_word)
+    print('There are {} "{}" in the text'.format(num_word, find_word))
